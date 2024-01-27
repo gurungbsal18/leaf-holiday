c2b2530f64222b83e4e288029a346a4b9308ae96
Bishal Gurung <gurungbsal18@gmail.com>
2024-01-27 20:01:52 +0545

    changes

--- src/app/(admin-routes)/admin/page.jsx
+++ src/app/(admin-routes)/admin/page.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function AdminDashboard() {
-  return <div>Admin Dashboard</div>;
+  return (
+    <>
+      <div className="dashboard-content-section p-4">Admin Dashboard</div>
+    </>
+  );
 }
