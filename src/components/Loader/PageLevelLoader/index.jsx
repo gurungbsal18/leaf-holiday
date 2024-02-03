593f7a024f6c949912fe9d27d6be6f13887f2951
Bishal Gurung <gurungbsal18@gmail.com>
2024-02-03 21:35:03 +0545

    loading ui updated

--- src/components/Loader/PageLevelLoader/index.jsx
+++ src/components/Loader/PageLevelLoader/index.jsx
@@ -1,15 +1,16 @@
 "use client";
 
-import { FadeLoader } from "react-spinners";
+import { BeatLoader } from "react-spinners";
 
 export default function PageLevelLoader({ loading }) {
   return (
-    <div className="d-flex justify-content-center align-items-center h-100">
-      <FadeLoader
-        color="#000000"
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      <BeatLoader
+        color="#36d7b7"
         loading={loading}
-        size={30}
+        size={10}
         data-testid="loader"
+        speedMultiplier={1}
       />
     </div>
   );
