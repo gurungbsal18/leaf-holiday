842c7a0883f17018d9e6ef2de9dc0292ccd02729
Bishal Gurung <gurungbsal18@gmail.com>
2024-02-15 20:36:13 +0545

    loader height fixed

--- src/components/Loader/PageLevelLoader/index.jsx
+++ src/components/Loader/PageLevelLoader/index.jsx
@@ -4,7 +4,7 @@ import { BeatLoader } from "react-spinners";
 
 export default function PageLevelLoader() {
   return (
-    <div className="d-flex justify-content-center align-items-center h-100vh">
+    <div className="d-flex justify-content-center align-items-center h-100">
       <BeatLoader
         color="#198754"
         loading={true}
