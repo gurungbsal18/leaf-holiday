9cc948049908ee93e958f92fc73b9b38e5776086
Bishal Gurung <gurungbsal18@gmail.com>
2024-02-16 01:16:23 +0545

    loader UI fixed

--- src/components/Loader/PageLevelLoader/index.jsx
+++ src/components/Loader/PageLevelLoader/index.jsx
@@ -4,7 +4,7 @@ import { BeatLoader } from "react-spinners";
 
 export default function PageLevelLoader() {
   return (
-    <div className="d-flex justify-content-center align-items-center vh-100">
+    <div className="d-flex justify-content-center align-items-center mx-0 my-auto">
       <BeatLoader
         color="#198754"
         loading={true}
