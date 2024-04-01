6875aa21e0e156ab29718693292c48e91c07e2a3
Bishal Gurung <gurungbsal18@gmail.com>
2024-04-02 00:30:41 +0545

    new pages added on client route

---
+++ src/app/(client-routes)/travel-info/electricity-in-nepal/page.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <div className="common-banner"></div>
+      <div className="container my-5">
+        <h1 className="title fw-bold">Electricity In Nepal</h1>
+        <p className="text-justify">
+          Electricity in Nepal, Residential electrical outlets in some countries
+          including the United States use 110-120 volts of electricity, and
+          accept very specific shaped plugs. Many other countries including
+          Nepal use other voltages, 220-240 volts to power their appliances, as
+          well different plugs. If you try to plug an American appliance such as
+          a shaver or hairdryer into an outlet of a different voltage, you may
+          destroy the appliance and cause yourself injury. There are few things
+          you should know about other countries (here Nepal) before you travel.
+        </p>
+        <p className="text-justify">
+          Nepal’s Electricity is 220-240 Volts and 50 HZ (some countries like
+          U.S and Canada are 110-120 volts/60 HZ). In Nepal electric Plugs are
+          of two or three round prongs, but not flat prongs as found in use in
+          the United States or in other countries.
+        </p>
+        <p className="text-justify">
+          If your appliance’s plug doesn’t match the shape of these sockets, you
+          will need a travel plug adapter in order to plug in. Travel plug
+          adapters simply change the shape of your appliance’s plug to match
+          whatever type of socket you need to plug into. If it’s crucial to be
+          able to plug in no matter what, bring an adapter for both types.
+        </p>
+        <p className="text-justify">
+          If you are planning on taking a hair dryer, electric shaver, curling
+          iron, laptop computer or other electrical devices on your travels, in
+          these cases, you will need to obtain either a voltage adapter
+          (electrical converter) or a plug adapter for your travels, most often
+          both. Most voltage adapters for travel convert 220-240 to 110-120,
+          usable by your American and similar appliance. A plug adapter, on the
+          other hand, allows you to plug into a foreign outlet.In Nepal, you
+          will need a voltage converter, and plug adapter in order to use U.S. –
+          like appliances.
+        </p>
+        <p className="text-justify">
+          In some countries you may only need one or the other (voltage
+          converter or plug adapter), but in Nepal you may require both. Note:
+          you may need many different plug adapters if your travels extend to
+          more than one country, but one voltage adapter is okay to work in one
+          country.
+        </p>
+        <p className="text-justify">
+          We recommend getting a universal adapter and converter kit if you plan
+          to take many electronics. It can be found in many hardware stores,
+          travel shops and online retailers. Virtually all laptop computers and
+          some electric razors take universal voltages. Check your equipment to
+          be sure. If the only electric device that you plan to take with you is
+          an electric shaver, you may consider buying a different model which
+          accepts universal voltages, to avoid carrying around a heavy and bulky
+          voltage adapter. Always be sure to check with the manufacturer of the
+          appliance for proper use and conversion.
+        </p>
+        <p className="text-justify">
+          Electricity flow is not smooth in Nepal. Electricity often goes off
+          and on. Voltage fluctuation is very common and it is advised that you
+          use an adapter with quality power surge protector for your
+          electronics.
+        </p>
+        <p className="text-justify">
+          Nepal has not been able to produce enough electricity to meet the high
+          demand, so in dry seasons there is power outage (load shedding) for
+          hours, however hotels and businesses cover their electrical needs via
+          fuel cells and generators.
+        </p>
+        <em className="text-muted">
+          Note: You can find any of the transformers, plug adopter and
+          converters to buy in Kathmandu and other major cities easily.
+        </em>
+      </div>
+    </div>
+  );
+};
+
+export default page;
