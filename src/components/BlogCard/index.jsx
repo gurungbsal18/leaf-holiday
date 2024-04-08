0c7ad7f6024be08fbc301d409021a26ff380be86
Bishal Gurung <gurungbsal18@gmail.com>
2024-04-08 16:23:13 +0545

    view all blog page updated

--- src/components/BlogCard/index.jsx
+++ src/components/BlogCard/index.jsx
@@ -4,17 +4,18 @@ import Link from "next/link";
 
 export default function BlogCard({ blogDetail }) {
   return (
-    <Link
-      href={`/blog/${blogDetail?.slug}`}
-      className="col-12 col-lg-4 border text-dark">
-      <Image
-        src={blogDetail?.imageUrl}
-        width={400}
-        height={300}
-        alt="blog-image"
-      />
-      <div className="px-3">
-        <h4 className="my-4 title">{blogDetail?.title}</h4>
+    <Link href={`/blog/${blogDetail?.slug}`} className="col-12 col-lg-4">
+      <div className="border text-dark">
+        <Image
+          src={blogDetail?.imageUrl}
+          width={400}
+          height={300}
+          alt="blog-image"
+          style={{width: "100%"}}
+        />
+        <div className="px-3">
+          <h4 className="my-4 title">{blogDetail?.title}</h4>
+        </div>
       </div>
     </Link>
   );
